3e323dded315b6248a1670a71dae0eebaab5d0e9
stijn <stijnsepter@gmail.com>
2023-09-13 10:29:53 +0200

    ik heb de assets bij gevoegt en de framewrok gemaakt

--- index.php
+++ index.php
@@ -1,2 +1,41 @@
 <?php
-echo test:
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Annexbios hoofdkantoor</title>
+    <link href="./css/mainstyle.css" rel="stylesheet" />
+</head>
+<body>
+    <nav class="navbar">
+        <div class="kop">
+            <img src="" alt="">
+        </div>
+        <div class="navbar_onderkant">
+            <h4 class="navbar_koop"> koop je tickets</h4>
+            <div class="seach"></div>
+            <!-- <button><h4> bestel je tickets</h4></button> -->
+        </div>
+    </nav>
+    <div class="header">
+
+    </div>
+    <div class="versigingen">
+
+    </div>
+    <div class="aanbevolenfilms_tussenkopje"><h2>AANBEVOLEN FILMS</h2></div>
+    <div class="aanbevolen_films"></div>
+    <div class="news_tussenkopje"></div>   
+    <div class="news"></div>
+    <footer>
+        <div class="footer">
+            <div class="footer_deelfoto"></div>
+        </div>
+        <div class="voorwaarden"></div>
+    </footer>
+</body>
+</html>
