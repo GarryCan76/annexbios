9a5d6b6d399239c6f61bcff3b904dd1c7a338bf4
stijn <stijnsepter@gmail.com>
2023-09-13 21:56:22 +0200

    eerste push front-end bijna klaar

--- index.php
+++ index.php
@@ -1,41 +1,69 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Annexbios hoofdkantoor</title>
-    <link href="./css/mainstyle.css" rel="stylesheet" />
+    <link href="mainstyle.css" rel="stylesheet" />
 </head>
 <body>
     <nav class="navbar">
         <div class="kop">
-            <img src="" alt="">
+            <img id="logo" src="./assets/logo/logo_hoofd.png" alt="logo">
+            <div class="kop_tekst">
+                <h3 class="kop1">VESTIGINGEN</h3>
+                <h3 class="kop2">AANBEVOLEN FILMS</h3>
+                <h3 class="kop3">CONTACT</h3>
+            </div>
         </div>
         <div class="navbar_onderkant">
-            <h4 class="navbar_koop"> koop je tickets</h4>
-            <div class="seach"></div>
-            <!-- <button><h4> bestel je tickets</h4></button> -->
+            <h2 class="navbar_koop"> KOOP JE TICKETS</h2>
+            <div class="seach"><p class="tekst_seach">Kies je vestiging</p></div>
+            <div class="bekijktickets"><h5 class="teks_bekijktickets">BEKIJK TICKETS</h5></div>
         </div>
     </nav>
-    <div class="header">
-
-    </div>
-    <div class="versigingen">
-
-    </div>
-    <div class="aanbevolenfilms_tussenkopje"><h2>AANBEVOLEN FILMS</h2></div>
-    <div class="aanbevolen_films"></div>
-    <div class="news_tussenkopje"></div>   
-    <div class="news"></div>
-    <footer>
-        <div class="footer">
-            <div class="footer_deelfoto"></div>
+        <div class="header">
+            <h1>WELKOM BIJ ANNEXBIOS</h1>
+            <p class="header_tekst">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit possimus eos fugit provident, eius perspiciatis facere veritatis rem <br> nihil nulla ullam nemo iusto fugiat excepturi vel velit aspernatur cum recusandae!</p>
+            <div class="header_button"><p class="header_knoptekst">BEKIJK ONZE VESTIGINGEN</p></div>
         </div>
-        <div class="voorwaarden"></div>
+        <div class="vestigingen">
+            <div class="vestiging1"></div>
+            <div class="vestiging1"></div>
+            <div class="vestiging1"></div>
+            <div class="vestiging1"></div>
+            <div class="vestiging1"></div>
+            <div class="vestiging1"></div>
+        </div>
+        <div class="aanbevolenfilms_tussenkopje">
+            <h2 class="aanbevolen_films_title">AANBEVOLEN FILMS</h2>
+            <div class="aanbevolen_films_div">
+                <div class="aanbevolen_films"></div>
+                <div class="aanbevolen_films"></div>
+                <div class="aanbevolen_films"></div>
+                <div class="aanbevolen_films"></div>
+                <div class="aanbevolen_films"></div>
+                <div class="aanbevolen_films"></div>
+            </div>
+        </div>
+
+        <!-- wij gaan het nieuws overslaan  -->
+        <div class="news_tussenkopje"></div>   
+        <div class="news"></div>
+
+    <footer id="footer">
+        <div class="footer_div1">
+            <img class="logo_footer" src="./assets/logo/MicrosoftTeams-image.png" alt="logo">
+            <div class="footer_tekstcontainer">
+            <p class="footer_tekst">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, accusamus! Facilis culpa excepturi commodi, accusantium laborum voluptas vero quas autem aliquam suscipit ipsum esse explicabo, veniam nihil eos eum eligendi.</p>
+            </div>
+            <div class="leestmeer">LEES MEER</div>
+        </div>
+        <div class="footer_div2">
+            <h2 class="navigeer_tekst">NAVIGEER</h2>
+        </div>
+        <div class="footer_div2"><div>
     </footer>
+    <div class="voorwaarden"> <p class="laatsetekst">Voorwaarden | Privacy beleid | Cookie disclaimer</p></div>
 </body>
 </html>
