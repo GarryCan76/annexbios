f161818fa2bb6717affdcbedefe87f3f9b10816b
stijn <stijnsepter@gmail.com>
2023-09-19 11:57:09 +0200

    test4

--- api/common/db.php
+++ api/common/db.php
@@ -4,10 +4,9 @@ $username = 'root';
 $password = '';
 $dbName = 'module_11';
 
-echo "test";
+
 $connection = mysqli_connect($servername, $username, $password, $dbName);
 
 if (!$connection){
     die('Database connection error : ' .mysqli_error());
 }
-echo "succes!";
